7cfc8fd8f0891eb6621809e43ff5e24dd64ee81b
jalcalaroot <johanalcala104@gmial.com>
2020-12-18 15:00:22 -0300

    multi region

--- variables.tf
+++ variables.tf
@@ -4,8 +4,14 @@ variable "secret_key" {}
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "us-east-1"
+  region     = "${var.region}"
 }
+
+variable "region" {
+  type        = "string"
+  description = "indicar el nombre del cluster"
+}
+
 
 variable "ecs_cluster" {
   type        = "string"
